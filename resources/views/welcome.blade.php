562f34fe668835ae7c69e5f075488a4ff6b93259
oppong-coffie <teama@teamalfy.com>
2024-11-15 08:22:01 +0000

    another

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TRY HOSTING</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
 </head>
 <body>
 
     <h1>HERE IS JUST FOR TRYING HOSTING OF LARAVEL </h1>
-    <p>
+    <div
+    data-aos="fade-up"
+    data-aos-offset="200"
+    data-aos-delay="50"
+    data-aos-duration="1000"
+    >
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
          Ipsam numquam perferendis voluptatem voluptatum soluta 
          velit asperiores quasi nam! Culpa facere, odio fugit enim
          reprehenderit vero dolores harum unde omnis iure.
-    </p>
+    </div>
     
+
+ <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
 </body>
 </html>
